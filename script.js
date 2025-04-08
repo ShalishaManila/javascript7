03e8ec986a3a189764836233bcee2499f4390c87
Gustav Liffner <81076368+GrumblePuddle@users.noreply.github.com>
2025-04-09 00:03:21 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
 // Variables for the DOM elements
+const word = document.getElementById("word");
+const text = document.getElementById("text");
+const scoreEl = document.getElementById("score");
+const timeEl = document.getElementById("time");
+const endgameEl = document.getElementById("end-game-container");
+const settings = document.getElementById("settings");
+const settingsForm = document.getElementById("settings-form");
+const settingsBtn = document.getElementById("settings-btn");
+const difficultySelect = document.getElementById("difficulty");
 
 // Array
 const words = [
