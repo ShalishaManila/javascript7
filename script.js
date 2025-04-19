2afdf05cea022ad0fbe60a9864e0d96db3636460
ShalishaManila <shalisha.manila@gmail.com>
2025-04-19 22:18:53 +0200

    updated functions

--- script.js
+++ script.js
@@ -9,6 +9,8 @@ const settingsForm = document.getElementById("settings-form");
 const settingsBtn = document.getElementById("settings-btn");
 const difficultySelect = document.getElementById("difficulty");
 
+const gameContainer = document.getElementsByClassName("container");
+
 // Array
 const words = [
   "dependent",
@@ -32,29 +34,127 @@ const words = [
   "north",
 ];
 
-// Functions
-function addWordToDOM(){}
+// General Functions
+// Start Game
+function startGame() {
+  intervalId = setInterval(updateTime, 1000);
+  addWordToDOM();
+}
 
-function updateScore(){}
+// Add radom word to guess
+function addWordToDOM() {
+  console.log("Generating New word...");
+  randomWord = words[Math.floor(Math.random() * words.length)];
+  word.textContent = randomWord;
+  console.log(randomWord);
+}
 
-function checkWord(e){}
+// Update score if guessed correct word
+function updateScore() {
+  score++;
+  console.log("updating score", score);
+  scoreEl.textContent = score;
+}
 
-function gameOver(){}
+// Game over if time out
+function gameOver() {
+  endgameEl.style.display = "flex";
+  endgameEl.innerHTML = "";
+  endgameEl.insertAdjacentHTML("beforeend", "<h1>Game Over</h1>");
+  endgameEl.insertAdjacentHTML(
+    "beforeend",
+    `<p>Difficulty Level: ${difficultySelect.value} </h1>`
+  );
+  endgameEl.insertAdjacentHTML("beforeend", `<p>Final Score: ${score} </h1>`);
+  //endgameEl.textContent = 'GAME OVER ... \n Time Out!!! \n Final Score: ', score, '\n Game Difficulty: ', difficultySelect.value;
 
-function updateTime(){}
+  endgameEl.insertAdjacentHTML(
+    "beforeend",
+    "<button onClick=resetGame()>Restart Game</button>"
+  );
+}
 
-// Event Listeners
-settingsBtn.addEventListener("click", function(){});
+// Reset Game
+function resetGame() {
+  randomWord = words[Math.floor(Math.random() * words.length)];
+  endgameEl.style.display = "none";
+  console.log("clearing interval!!!");
+  clearInterval(intervalId);
+  switch (difficultySelect.value) {
+    case "easy":
+      time = 10;
+      break;
+    case "medium":
+      time = 7;
+      break;
+    case "hard":
+      time = 5;
+      break;
+    default:
+      time = 10;
+      break;
+  }
+  score = 0;
+  console.log("score cleared", score);
+  scoreEl.textContent = score;
+  startGame();
+}
 
-difficultySelect.addEventListener("change", function(){});
+// Listener Functions
+// When settings button is clicked
+function settingsBtn_Click() {
+  if (settings.style.visibility === "hidden") {
+    settings.style.visibility = "visible";
+  } else {
+    settings.style.visibility = "hidden";
+  }
+}
+
+// Update time
+function updateTime() {
+  time--;
+  timeEl.textContent = time + "s";
+  if (time <= 0) {
+    clearInterval(intervalId);
+    gameOver();
+  }
+}
+
+// When difficulty selection changes
+function difficultySelect_Change() {
+  console.log(difficultySelect.value);
+  selectedDifficulty = difficultySelect.value;
+}
+
+// Check if correct word is guessed
+function checkWord(e) {
+  if (e.key === "Enter") {
+    console.log(text.value);
+    if (text.value === word.textContent) {
+      console.log("Correct word gussed");
+      updateScore();
+      addWordToDOM();
+      text.value = "";
+      time = time + 5;
+      timeEl.textContent = time + "s";
+    }
+  }
+}
+
+settingsBtn.addEventListener("click", settingsBtn_Click);
+
+difficultySelect.addEventListener("change", difficultySelect_Change);
 
 text.addEventListener("keypress", checkWord);
 
 //Initializing word
-let randomWord;
+let randomWord = words[Math.floor(Math.random() * words.length)];
 
 //Initializing score
 let score = 0;
 
 //Initializing time
 let time = 10;
+let intervalId;
+
+startGame();
